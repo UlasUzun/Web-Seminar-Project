d0e292e244306d9fb577db3443309b4231ef6fba
Sefa <148440837+SefaBayrak@users.noreply.github.com>
2026-05-09 20:51:06 +0200

    Added Responsive CSS and view layouts

---
+++ views/footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer>
+    <div class="footer-content">
+        <p>&copy; <?php echo date('Y'); ?> Animal Kingdom</p>
+    </div>
+</footer>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
